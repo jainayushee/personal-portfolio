36bd97d8ba4479bc32e73711ddd008ab9eb775e2
jainayushee <jainayushee5@gmail.com>
2022-09-13 22:09:43 +0530

    Add resume + linting fixes

--- main.js
+++ main.js
@@ -1,42 +1,28 @@
-function openResume() {
-    var fileUrl = "data:image/png;base64," + bytesBase64;
-fetch(fileUrl)
-    .then(response => response.blob())
-    .then(blob => {
-        var link = window.document.createElement("a");
-        link.href = window.URL.createObjectURL(blob, { type: mimeType });
-        link.download = fileName;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    });
-}
-
 const goTo = (website) => {
-    
-    switch(website) {
-        case "airlineGithub" : window.open("https://github.com/jainayushee/airline-landing-page", "_blank")
-        break;
-        case "airlineLive" : window.open("http://angula48998r.s3-website.ap-south-1.amazonaws.com/", "_blank")
-        break;
-        case "keepGithub" : window.open("http://www.google.com", "_blank")
-        break;
-        case "keepLive" : window.open("http://www.google.com", "_blank")
-        break;
-        case "trolleyLive" : window.open("https://drive.google.com/file/d/1MN_vtPYZLgQ-UIIQw-jmdch5K9WVyAsG/view", "_blank")
-        break;
-        case "weatherGithub" : window.open("https://github.com/jainayushee/weather-app", "_blank")
-        break;
-        case "weatherLive" : window.open("https://rapid-weather.netlify.app/", "_blank")
-        break;
-        case "calculatorGithub" : window.open("https://github.com/jainayushee/mobileCalculator", "_blank")
-        break;
-        case "todoGithub" : window.open("https://github.com/jainayushee/To-Do-List", "_blank")
-        break;
-        case "travelGithub" : window.open("https://github.com/jainayushee/Travel-Guide", "_blank")
-        break;
-        case "t2sGithub" : window.open("https://github.com/jainayushee/mobileTextToSpeech", "_blank")
-        break;
+
+    switch (website) {
+        case "airlineGithub": window.open("https://github.com/jainayushee/airline-landing-page", "_blank")
+            break;
+        case "airlineLive": window.open("http://angula48998r.s3-website.ap-south-1.amazonaws.com/", "_blank")
+            break;
+        case "keepGithub": window.open("http://www.google.com", "_blank")
+            break;
+        case "keepLive": window.open("http://www.google.com", "_blank")
+            break;
+        case "trolleyLive": window.open("https://drive.google.com/file/d/1MN_vtPYZLgQ-UIIQw-jmdch5K9WVyAsG/view", "_blank")
+            break;
+        case "weatherGithub": window.open("https://github.com/jainayushee/weather-app", "_blank")
+            break;
+        case "weatherLive": window.open("https://rapid-weather.netlify.app/", "_blank")
+            break;
+        case "calculatorGithub": window.open("https://github.com/jainayushee/mobileCalculator", "_blank")
+            break;
+        case "todoGithub": window.open("https://github.com/jainayushee/To-Do-List", "_blank")
+            break;
+        case "travelGithub": window.open("https://github.com/jainayushee/Travel-Guide", "_blank")
+            break;
+        case "t2sGithub": window.open("https://github.com/jainayushee/mobileTextToSpeech", "_blank")
+            break;
     }
 
 }
